d43247d99e1497daac0eb167ea7406f6b2d4a3f8
jahnueldorelus <jahnueldorelus@gmail.com>
2023-01-04 07:14:15 -0500

    Refactored database model and route 	- Removed unused method from the services database model. 	- Updated the services route which now returns all the data of a service instead of excluding specific fields.

--- src/types-and-schemas/database/models/services/index.ts
+++ src/types-and-schemas/database/models/services/index.ts
@@ -1,9 +1,5 @@
 import { HydratedDocument, Model } from "mongoose";
 
-// Makes some properties of the service's data optional to allow deletion
-export type PrivateServiceData = Partial<IService> &
-  Omit<IService, "portNumber" | "api">;
-
 export type IService = {
   name: string;
   api: string;
@@ -13,12 +9,7 @@ export type IService = {
   available: boolean;
 };
 
-export type IServiceMethods = {
-  /**
-   * Generates a JSON that excludes a service's private information.
-   */
-  toPrivateJSON(): PrivateServiceData;
-};
+export type IServiceMethods = {};
 
 export interface ServicesModel extends Model<IService, {}, IServiceMethods> {}
 
